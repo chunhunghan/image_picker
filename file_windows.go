1daa456710b8522a332dedffda3d31b88eed8655
chunhunghan <chunhunghan@gmail.com>
2019-07-22 11:31:04 +0800

    filter for windows

--- file_windows.go
+++ file_windows.go
@@ -1,11 +1,69 @@
+// +build windows,!linux,!darwin,!js
+
 package image_picker
 
 import (
+	"strings"
 	"syscall"
+	"unicode/utf16"
 	"unsafe"
 
 	"github.com/pkg/errors"
 )
+
+var (
+	comdlg32         = syscall.NewLazyDLL("comdlg32.dll")
+	getOpenFileNameW = comdlg32.NewProc("GetOpenFileNameW")
+)
+
+const (
+	maxPath          = 260
+	ofnExplorer      = 0x00080000
+	ofnFileMustExist = 0x00001000
+	ofnHideReadOnly  = 0x00000004
+)
+
+type openfilenameW struct {
+	lStructSize       uint32
+	hwndOwner         syscall.Handle
+	hInstance         syscall.Handle
+	lpstrFilter       *uint16
+	lpstrCustomFilter *uint16
+	nMaxCustFilter    uint32
+	nFilterIndex      uint32
+	lpstrFile         *uint16
+	nMaxFile          uint32
+	lpstrFileTitle    *uint16
+	nMaxFileTitle     uint32
+	lpstrInitialDir   *uint16
+	lpstrTitle        *uint16
+	flags             uint32
+	nFileOffset       uint16
+	nFileExtension    uint16
+	lpstrDefExt       *uint16
+	lCustData         uintptr
+	lpfnHook          syscall.Handle
+	lpTemplateName    *uint16
+	pvReserved        unsafe.Pointer
+	dwReserved        uint32
+	flagsEx           uint32
+}
+
+func utf16PtrFromString(s string) *uint16 {
+	b := utf16.Encode([]rune(s))
+	return &b[0]
+}
+
+func stringFromUtf16Ptr(p *uint16) string {
+	b := *(*[maxPath]uint16)(unsafe.Pointer(p))
+	r := utf16.Decode(b[:])
+	return strings.Trim(string(r), "\x00")
+}
+
+func getOpenFileName(lpofn *openfilenameW) bool {
+	ret, _, _ := getOpenFileNameW.Call(uintptr(unsafe.Pointer(lpofn)), 0, 0)
+	return ret != 0
+}
 
 func fileDialog(title string, fileType string) (string, error) {
 	var ofn openfilenameW
@@ -21,9 +79,9 @@ func fileDialog(title string, fileType string) (string, error) {
 	var filters string
 	switch fileType {
 	case "image":
-		filters = `*.png *.jpg *.jpeg`
+		filters = "Image Files\0*.png;*.jpg;*.jpeg\0"
 	case "video":
-		filters = `*.webm *.mpeg *.mkv *.mp4 *.avi *.mov *.flv`
+		filters = "Video Files\0*.webm;*.mpeg;*.mkv;*.mp4;*.avi;*.mov;*.flv\0"
 	default:
 		return "", errors.New("unsupported fileType")
 	}
