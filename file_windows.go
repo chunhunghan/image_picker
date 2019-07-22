c552959b65f50343fff02262f41fecd65aa21584
chunhunghan <chunhunghan@gmail.com>
2019-07-22 12:08:43 +0800

    rollback

--- file_windows.go
+++ file_windows.go
@@ -79,7 +79,7 @@ func fileDialog(title string, fileType string) (string, error) {
 	var filters string
 	switch fileType {
 	case "image":
-		filters = "Image Files(*.png,*.jpg,*.jpeg)|*.png;*.jpg;*.jpeg||"
+		filters = "Image Files(*.png,*.jpg,*.jpeg)\n*.png;*.jpg;*.jpeg\n\n"
 	case "video":
 		filters = "Video Files\\0*.webm;*.mpeg;*.mkv;*.mp4;*.avi;*.mov;*.flv\\0"
 	default:
